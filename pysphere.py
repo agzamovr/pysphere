fb5186247b7d518a5df28caab9d5123eb7247ad5
Marco Faedo <marcofaedo@gmail.com>
2015-07-02 16:59:50 +0200

    changed method signatures

--- pysphere.py
+++ pysphere.py
@@ -1,62 +1,52 @@
 from math import radians, degrees, cos, sin, asin, acos, sqrt, atan2
 
-def haversine(lat1, lon1, lat2, lon2, r = 6371):
+def convert2radians(p):
+    return map(radians, p)
+
+def haversine(p1, p2, r = 6371):
     """
     Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    Radius of earth in kilometers. Use r = 3956 for miles
+    on the earth: p1 and p2 (specified in decimal degrees)
+    Points must be passed as (lat, lon)
+    r is the radius of earth (default radius in kms)
+    Use r = 3956 for miles
     """
-
-    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
-    dlat = lat2 - lat1;
-    dlon = lon2 - lon1;
-
+    ((lat1, lon1), (lat2, lon2))    = map(convert2radians, (p1, p2))
+    dlat, dlon                      = (lat2 - lat1, lon2 - lon1)
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * atan2(sqrt(a), sqrt(1-a))
-    d = r * c;
-
-    return d;
+    return r * c;
 
 
-def bearing(lat1, lon1, lat2, lon2):
+def bearing(p1, p2):
     """
-    Initial bearing (sometimes referred to as forward azimuth) which if
-    followed in a straight line along a great-circle arc will take you from
-    the point 1 to the point 2
+    Initial bearing (sometimes referred to as forward azimuth) which if followed
+    in a straight line along a great-circle arc will take you from p1 to p2
     """
-    # convert decimal degrees to radians
-    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
-
+    ((lat1, lon1), (lat2, lon2))    = map(convert2radians, (p1, p2))
     x = sin(lon2-lon1)*cos(lat2)
     y = cos(lat1)*sin(lat2)-sin(lat1)*cos(lat2)*cos(lon2-lon1)
-
     return degrees(atan2(x, y))
 
-def cross_track_distance(lat1, lon1, lat_start, lon_start, lat_end, lon_end, r = 6371):
+def cross_track_distance(p_start, p_end, p, r = 6371):
     """
-    Distance of a point A from a great-circle path (sometimes called cross track error).
-    lat1, lon1: coordinates of point A
+    Distance of a point p from a great-circle path (sometimes called cross track error).
     """
-    # # d13 is (angular) distance from start point to point A
-    d13 = haversine(lat_start, lon_start, lat1, lon1, r) / r
-    # # t13 is (initial) bearing from start point to point
-    t13 = radians(bearing(lat_start, lon_start, lat1, lon1))
-    # # t12 is (initial) bearing from start point to end point
-    t12 = radians(bearing(lat_start, lon_start, lat_end, lon_end))
-
+    # d13 is (angular) distance from start point to point A
+    # t13 is (initial) bearing from start point to point
+    # t12 is (initial) bearing from start point to end point
+    d13 = haversine(p_start, p, r) / r
+    t13 = radians(bearing(p_start, p))
+    t12 = radians(bearing(p_start, p_end))
     return asin(sin(d13) * sin(t13-t12)) * r;
 
-def along_track_distance(lat1, lon1, lat_start, lon_start, lat_end, lon_end, r = 6371):
+def along_track_distance(p_start, p_end, p, r = 6371):
     """
     The along-track distance is the distance from the start point to the
     closest point to point A, on the path to the end point.
     """
-
     # d13 is (angular) distance from start point to point A
-    d13 = haversine(lat_start, lon_start, lat1, lon1, r) / r
     # dxt is (angular) cross-track distance
-    dxt = cross_track_distance(lat1, lon1, lat_start, lon_start, lat_end, lon_end, r) / r
-
-    dat = acos(cos(d13)/cos(dxt)) * r
-
-    return dat;
+    d13 = haversine(p_start, p, r) / r
+    dxt = cross_track_distance(p_start, p_end, p, r) / r
+    return acos(cos(d13)/cos(dxt)) * r;
--- unittests.py
+++ unittests.py
@@ -3,19 +3,33 @@ import pysphere
 
 class TestModule(unittest.TestCase):
   def test_haversine_00(self):
-      self.assertAlmostEqual(pysphere.haversine(52.516288, 13.377689, 52.519198, 13.379453), 0.3448896)
+      self.assertAlmostEqual(pysphere.haversine(p1 = (52.516288, 13.377689), p2 = (52.519198, 13.379453)), 0.3448896)
 
   def test_haversine_01(self):
-      self.assertAlmostEqual(pysphere.haversine(52.516288, 13.377689, 52.516288, 13.377689), 0)
+      self.assertAlmostEqual(pysphere.haversine(p1 = (52.516288, 13.377689), p2 = (52.516288, 13.377689)), 0)
 
-  def test_bearing(self):
-      self.assertAlmostEqual(pysphere.bearing(52.516288, 13.377689, 52.519198, 13.379453), 20.24698, places=5)
+  def test_haversine_02(self):
+      p1 = (52.516288, 13.377689)
+      p2 = (52.519198, 13.379453)
+      self.assertAlmostEqual(pysphere.haversine(p1, p2), pysphere.haversine(p2, p1))
+
+  def test_bearing_00(self):
+      self.assertAlmostEqual(pysphere.bearing((52.516288, 13.377689), (52.519198, 13.379453)), 20.24698, places=5)
+
+  def test_bearing_01(self):
+      self.assertAlmostEqual(pysphere.bearing(p2 = (52.519198, 13.379453), p1 = (52.516288, 13.377689)), 20.24698, places=5)
 
   def test_cross_track_distance(self):
-      self.assertAlmostEqual(pysphere.cross_track_distance(52.516288, 13.377689, 52.590117, 13.39915, 52.437385, 13.553989), 5.552993, places=6)
+      p = (52.516288, 13.377689)
+      p_start = (52.590117, 13.39915)
+      p_end = (52.437385, 13.553989)
+      self.assertAlmostEqual(pysphere.cross_track_distance(p_start, p_end, p), 5.552993, places=6)
 
   def test_along_track_distance(self):
-      self.assertAlmostEqual(pysphere.along_track_distance(52.516288, 13.377689, 52.590117, 13.39915, 52.437385, 13.553989), 6.218037, places=6)
+      p = (52.516288, 13.377689)
+      p_start = (52.590117, 13.39915)
+      p_end = (52.437385, 13.553989)
+      self.assertAlmostEqual(pysphere.along_track_distance(p_start, p_end, p), 6.218037, places=6)
 
 if __name__ == '__main__':
     unittest.main()
